496b2d0b34e220678a3f314d5aa317dfe2dd884f
Melvin Lee <melvin.lee@iswish.nl>
2025-10-09 16:08:36 +0200

    chore(oas): Gebeurtenissen Bevragen API specificatie

--- scripts/oas-resolve.sh
+++ scripts/oas-resolve.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 npx redocly bundle ./specificaties/gebeurtenissen-publiceren-service/openapi.yaml -o ./specificaties/gebeurtenissen-publiceren-service/resolved/openapi.yaml
+npx redocly bundle ./specificaties/gebeurtenissen-bevragen-service/openapi.yaml -o ./specificaties/gebeurtenissen-bevragen-service/resolved/openapi.yaml
