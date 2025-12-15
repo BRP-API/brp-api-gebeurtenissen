1ed692da4594726cb2adc011bb484d69cecdd57f
Melvin Lee <melvin.lee@iswish.nl>
2025-12-15 16:23:56 +0100

    Gebeurtenissen Publiceren API specificatie (#29)

--- scripts/oas-resolve.sh
+++ scripts/oas-resolve.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+npx redocly bundle ./specificaties/gebeurtenissen-publiceren-service/openapi.yaml -o ./specificaties/gebeurtenissen-publiceren-service/resolved/openapi.yaml
 npx redocly bundle ./specificaties/abonnementen-service/openapi.yaml -o ./specificaties/abonnementen-service/resolved/openapi.yaml
 npx redocly bundle ./specificaties/mutatie-service/openapi.yaml -o ./specificaties/mutatie-service/resolved/openapi.yaml
