dd0ae9d5522bad7f4bb4bd97d1c6917945a3d975
Melvin Lee <melvin.lee@iswish.nl>
2025-12-15 12:18:52 +0100

    Getting started (#52)

---
+++ scripts/containers-stop.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker compose \
+    -f ./.docker/axon-server.yml \
+    -f ./.docker/gebeurtenissen-publiceren-service.yml \
+    -f ./.docker/gebeurtenissen-bevragen-service.yml \
+    down
