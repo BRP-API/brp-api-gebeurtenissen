284d1ffee93c0c7902d5e88dfb7a73fbe8209f30
nkorporaal <nap.korporaal+github@gmail.com>
2025-12-15 16:58:54 +0100

    chore: refactor Docker setup

--- scripts/containers-stop.sh
+++ scripts/containers-stop.sh
@@ -2,6 +2,10 @@
 
 docker compose \
     -f ./.docker/axon-server.yml \
+    -f ./.docker/db.yml \
     -f ./.docker/gebeurtenissen-publiceren-service.yml \
     -f ./.docker/gebeurtenissen-bevragen-service.yml \
+    -f ./.docker/gebeurtenissen-mutatie-service.yml \
+    -f ./.docker/gebeurtenissen-abonneren-service.yml \
+    -f ./.docker/keycloak.yml \
     down
