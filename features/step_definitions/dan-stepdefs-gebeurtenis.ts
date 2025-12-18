007fc5dec37fdcc9ff9b1c35304747777124a28d
Melvin Lee <melvin.lee@iswish.nl>
2025-12-18 15:49:06 +0100

    fix(automation): dan is gebeurtenis gepubliceerd stap bepaalt gebeurtenis values adhv de uitgevoerde command

--- features/step_definitions/dan-stepdefs-gebeurtenis.ts
+++ features/step_definitions/dan-stepdefs-gebeurtenis.ts
@@ -3,13 +3,31 @@ import { CloudEvent } from './support/cloud-events';
 import { setNestedProperty } from './support/object-utils';
 import { Aanduiding } from './support/aanduiding';
 import { VerhuisdIntergemeentelijkEvent } from './brp/verhuisd-intergemeentelijk-event';
+import { AangifteVanAdreswijzigingCommand } from './brp-api/commands';
+import { Persoon } from './brp/persoon-entity';
 
 Then('zijn er geen gebeurtenissen gepubliceerd', function () {
 });
 
+function getPersoonByBsn(personen: any, bsn: string) : Persoon | undefined {
+    const key = Object.keys(personen).find(key => {
+        return personen[key].burger_service_nr === bsn;
+    });
+    return key ? personen[key] : undefined;
+}
+
 Then('is een {string} gebeurtenis gepubliceerd( met de volgende velden)( met de volgende data)', function (gebeurtenisType: string) {
     this.expected = gebeurtenisType === 'verhuisd.intergemeentelijk' ? new VerhuisdIntergemeentelijkEvent(true) : new CloudEvent(`nl.brp.${gebeurtenisType}`);
     this.aanduiding = Aanduiding.gepubliceerdGebeurtenis();
+
+    if(this.command instanceof AangifteVanAdreswijzigingCommand) {
+        const persoon: Persoon | undefined = getPersoonByBsn(this.context.personen, this.command.burgerservicenummer!);
+        if(persoon) {
+            this.expected.setAnummer(persoon.a_nr);
+        }
+        this.expected.setVerhuisdatum(this.command.verhuisdatum);
+        this.expected.setAdresseerbaarObjectIdentificatie(this.command.adresseerbaarObjectIdentificatie);
+    }
 });
 
 Then('is een {string} gebeurtenis geleverd( met de volgende velden)( met de volgende data)', function (gebeurtenisType: string) {
