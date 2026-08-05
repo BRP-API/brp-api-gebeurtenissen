c6edaf6144d6c1555cab619322325c3b57959178
Leroy Truong <leroytruong@cupofcode.io>
2026-08-05 13:00:43 +0200

    222 test: let `afnemerId` be a random UUID to avoid concurrency issues
    Concurrency issues lead to `ConsistencyException` by Axon.

--- features/step_definitions/support/afnemer-factory.ts
+++ features/step_definitions/support/afnemer-factory.ts
@@ -20,9 +20,7 @@ export class AfnemerFactory {
     let afnemer = context.afnemers[aanduiding];
     if (!afnemer) {
       afnemer = new Afnemer(aanduiding);
-      afnemer.afnemerId = (Object.keys(context.afnemers).length + 1)
-        .toString()
-        .padStart(6, '0');
+      afnemer.afnemerId = crypto.randomUUID()
       afnemer.oin = `00000009900${afnemer.afnemerId}0000`;
       if (gemeenteCodes.has(aanduiding)) {
         afnemer.gemeenteCode = gemeenteCodes.get(aanduiding);
