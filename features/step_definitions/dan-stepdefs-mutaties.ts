d535646acaa4a44cc47b68015e65d51214589da7
Melvin Lee <melvin.lee@iswish.nl>
2026-09-23 08:06:09 +0200

    chore(gts): fix lint errors

--- features/step_definitions/dan-stepdefs-mutaties.ts
+++ features/step_definitions/dan-stepdefs-mutaties.ts
@@ -54,25 +54,32 @@ Then('is de response een AdresGeregistreerd response', function () {
   expect(this.result.type).to.equal('AdresGeregistreerd');
 });
 
-Then('is het adres geregisteerd in de BRP met een unieke adresseerbaar object identificatie en de gemeentecode van {string}', async function (gemeente: string) {
-  const gemeentecode = gemeenteCodeMap[gemeente] || gemeente;
-  const statement = createSelectStatement(
-    'lo3_adres',
-    ['adres_id', 'verblijf_plaats_ident_code', 'gemeente_code'],
-    [this.result.adresId, this.result.adresseerbaarObjectIdentificatie, gemeentecode],
-  );
+Then(
+  'is het adres geregisteerd in de BRP met een unieke adresseerbaar object identificatie en de gemeentecode van {string}',
+  async function (gemeente: string) {
+    const gemeentecode = gemeenteCodeMap[gemeente] || gemeente;
+    const statement = createSelectStatement(
+      'lo3_adres',
+      ['adres_id', 'verblijf_plaats_ident_code', 'gemeente_code'],
+      [
+        this.result.adresId,
+        this.result.adresseerbaarObjectIdentificatie,
+        gemeentecode,
+      ],
+    );
 
-  const result = await PostgresqlManager.getInstance().execute(statement);
-  const actual = Object.fromEntries(result);
+    const result = await PostgresqlManager.getInstance().execute(statement);
+    const actual = Object.fromEntries(result);
 
-  logger.debug('Query database:', {
-    statement: statement,
-    result: actual,
-  });
+    logger.debug('Query database:', {
+      statement: statement,
+      result: actual,
+    });
 
-  expect(Number(actual.adres_id)).to.equal(this.result.adresId);
-  expect(actual.verblijf_plaats_ident_code).to.equal(
-    this.result.adresseerbaarObjectIdentificatie,
-  );
-  expect(actual.gemeente_code).to.equal(Number(gemeentecode));
-});
+    expect(Number(actual.adres_id)).to.equal(this.result.adresId);
+    expect(actual.verblijf_plaats_ident_code).to.equal(
+      this.result.adresseerbaarObjectIdentificatie,
+    );
+    expect(actual.gemeente_code).to.equal(Number(gemeentecode));
+  },
+);
--- features/step_definitions/support/gemeente-codes.ts
+++ features/step_definitions/support/gemeente-codes.ts
@@ -1,8 +1,8 @@
 export const gemeenteCodeMap: {[key: string]: string} = {
-    Amsterdam: '0363',
-    'Den Haag': '0518',
-    Hengelo: '0164',
-    Roosendaal: '1674',
-    Rotterdam: '0599',
-    Utrecht: '0344',
+  Amsterdam: '0363',
+  'Den Haag': '0518',
+  Hengelo: '0164',
+  Roosendaal: '1674',
+  Rotterdam: '0599',
+  Utrecht: '0344',
 };
